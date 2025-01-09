e9f962a5945c133840e61c064c6051e4aae57edb
Chirag <Khanna.Chirag@bcg.com>
2025-01-10 01:00:17 +0530

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,27 @@ import { UsersModule } from './users/users.module';
 import { DocumentsModule } from './documents/documents.module';
 import { IngestionModule } from './ingestion/ingestion.module';
 import { BaseAppModule } from './app/app.module';
+import { ConfigModule, ConfigService } from './config';
+import { TypeOrmModule, TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          entities: [__dirname + './../**/**.entity{.ts,.js}'],
+          synchronize: configService.get('DB_SYNC') === 'true',
+        } as TypeOrmModuleAsyncOptions;
+      },
+    }),
     AuthModule,
     UsersModule,
     DocumentsModule,
