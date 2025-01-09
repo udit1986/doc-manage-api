ed75937a6ea760dad78d5fde420ccafbeb48cbf8
Chirag <Khanna.Chirag@bcg.com>
2025-01-09 22:28:25 +0530

    fix: the login or register payload model

--- src/common/dto/register.dto.ts
+++ src/common/dto/register.dto.ts
@@ -1,5 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
 export class RegisterDto {
-  username: string;
+  @ApiProperty({
+    required: true,
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  firstName: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  lastName: string;
+
+  @ApiProperty({
+    required: true,
+  })
+  @IsNotEmpty()
+  @MinLength(5)
   password: string;
+
   role: string;
 }
